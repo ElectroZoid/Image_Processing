2776aeca5fa5e8c44622cf7eb8c3a057a4763ebd
Vishal-ElectroZoid <vishalrahangdale699@gmail.com>
2024-06-29 22:42:22 +0530

    Added default/custom name feature for the newfile

--- file_read.c
+++ file_read.c
@@ -30,8 +30,31 @@ int main(int argc, char **argv)
     fscanf(oldFile, "%d", &numCol);
     fscanf(oldFile, "%d", &numRow);
     fscanf(oldFile, "%d", &maxNum);
+        
+    /* If name of output file is not specified,
+        new name of file would be oldfile_name+OUT+.ppm */
+    
+    if (argc==3){
+        char newFile_name[strlen(argv[1])+4];
+        int j=0;
+        for (int i=0;i<strlen(argv[1]);i++){
+            if (argv[1][i]=='.'){
+                newFile_name[j++]='O';
+                newFile_name[j++]='u';
+                newFile_name[j++]='t';
+                newFile_name[j++]=argv[1][i];
+            }
+            else{
+                newFile_name[j++]=argv[1][i];
+            }
+        }
+        newFile_name[j]='\0';
+        newFile=fopen(newFile_name,"wb");        
+    }
+    else{
+        newFile=fopen(argv[2],"wb");
+    }
 
-    newFile=fopen("Treenew.ppm","wb");
     fprintf(newFile,"P6\n");
     fprintf(newFile,"%d %d\n",numCol,numRow);
     fprintf(newFile,"%d\n",maxNum);
